30e89049491c07cf2bc23eb03186a6aaf37f9758
“liton-ahmed” <litonahmed066@gmail.com>
2024-01-17 00:31:25 +0600

    one file is added

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+export default function Board() {
+  return (
+    <>
+      <div>
+      <button className="bg-white border border-gray-400 h-12 w-12 m-1 text-lg leading-9"> X</button>
+      <button className="bg-white border border-gray-400 h-12 w-12 m-1 text-lg leading-9"> X</button>
+      <button className="bg-white border border-gray-400 h-12 w-12 m-1 text-lg leading-9"> X</button>
+      </div>
+      <div>
+      <button className="bg-white border border-gray-400 h-12 w-12 m-1 text-lg leading-9"> X</button>
+      <button className="bg-white border border-gray-400 h-12 w-12 m-1 text-lg leading-9"> X</button>
+      <button className="bg-white border border-gray-400 h-12 w-12 m-1 text-lg leading-9"> X</button>
+      </div> 
+      <div>
+      <button className="bg-white border border-gray-400 h-12 w-12 m-1 text-lg leading-9"> X</button>
+      <button className="bg-white border border-gray-400 h-12 w-12 m-1 text-lg leading-9"> X</button>
+      <button className="bg-white border border-gray-400 h-12 w-12 m-1 text-lg leading-9"> X</button>
+      </div>
+    </>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsj}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
