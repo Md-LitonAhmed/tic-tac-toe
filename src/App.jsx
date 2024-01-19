a36fc703c922abef27c003a6c6cae74d2e3b8257
“liton-ahmed” <litonahmed066@gmail.com>
2024-01-20 01:35:25 +0600

    X and 0 inserted successfully

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,54 @@
+import { useState } from "react";
+
+function Square(props) {
+  const { value, onSquareClick } = props;
+
+  return (
+    <button
+      onClick={onSquareClick}
+      className="bg-white border border-gray-400 h-12 w-12 m-1 text-lg leading-9"
+    >
+      {" "}
+      {value}
+    </button>
+  );
+}
+
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXisNext] = useState(true);
+  const handleClick = (i) => {
+    if(squares[i]){
+      return;
+    }
+    const nextSquare = squares.slice();
+    if (xIsNext) {
+      nextSquare[i] = "X";
+    } else {
+      nextSquare[i] = "0";
+    }
+    setSquares(nextSquare);
+    setXisNext(!xIsNext);
+  };
+
   return (
     <>
-      <div>
-      <button className="bg-white border border-gray-400 h-12 w-12 m-1 text-lg leading-9"> X</button>
-      <button className="bg-white border border-gray-400 h-12 w-12 m-1 text-lg leading-9"> X</button>
-      <button className="bg-white border border-gray-400 h-12 w-12 m-1 text-lg leading-9"> X</button>
+      <div className="flex">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
-      <div>
-      <button className="bg-white border border-gray-400 h-12 w-12 m-1 text-lg leading-9"> X</button>
-      <button className="bg-white border border-gray-400 h-12 w-12 m-1 text-lg leading-9"> X</button>
-      <button className="bg-white border border-gray-400 h-12 w-12 m-1 text-lg leading-9"> X</button>
-      </div> 
-      <div>
-      <button className="bg-white border border-gray-400 h-12 w-12 m-1 text-lg leading-9"> X</button>
-      <button className="bg-white border border-gray-400 h-12 w-12 m-1 text-lg leading-9"> X</button>
-      <button className="bg-white border border-gray-400 h-12 w-12 m-1 text-lg leading-9"> X</button>
+
+      <div className="flex">
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+      </div>
+
+      <div className="flex">
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
   );
